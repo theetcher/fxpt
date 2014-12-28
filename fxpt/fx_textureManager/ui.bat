c60c1012dddc17ff3fae4adebbdae908a99b1629
Eugene Davydenko <etchermail@gmail.com>
2014-12-28 16:15:47 +0400

    textureManager: MainWindow UI done

--- fxpt/fx_textureManager/ui.bat
+++ fxpt/fx_textureManager/ui.bat
@@ -1,2 +1,2 @@
 C:\Python27\python.exe "C:\Python27\Lib\site-packages\PySide\scripts\uic.py" -o MainWindowUI.py MainWindowUI.ui
-rem C:\Python27\Lib\site-packages\PySide\pyside-rcc.exe resources.qrc -o resources_rc.py
+C:\Python27\Lib\site-packages\PySide\pyside-rcc.exe resources.qrc -o resources_rc.py
