afede111b8de30fcb3339e0a2b6371501115476e
Eugene Davydenko <etchermail@gmail.com>
2016-02-14 21:48:48 +0300

    env_print python version

---
+++ fxpt/fx_utils/env_print.py
@@ -0,0 +1,27 @@
+import os
+
+ENV_VARS = (
+    'MAYA_APP_DIR',
+    'MAYA_LOCATION',
+    'MAYA_SCRIPT_PATH',
+    'PYTHONPATH',
+    'MAYA_MODULE_PATH',
+    'MAYA_PLUG_IN_PATH',
+    'PATH',
+    'TMPDIR',
+    'XBMLANGPATH'
+)
+
+OFFSET = '    '
+
+
+def env_print():
+    print
+    for env in ENV_VARS:
+        print env
+        value = os.environ.get(env, None)
+        if value is None:
+            print OFFSET + 'variable not set.'
+        else:
+            for p in sorted(value.split(';')):
+                print OFFSET + p.strip()
