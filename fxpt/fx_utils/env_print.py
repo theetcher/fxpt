053cefa219c5af7e683d3488ede47487b1bfdf00
Eugene Davydenko <etchermail@gmail.com>
2016-02-19 01:43:41 +0300

    env_print supports internal vars

--- fxpt/fx_utils/env_print.py
+++ fxpt/fx_utils/env_print.py
@@ -1,4 +1,5 @@
 import os
+import maya.cmds as m
 
 ENV_VARS = (
     'MAYA_APP_DIR',
@@ -7,9 +8,23 @@ ENV_VARS = (
     'PYTHONPATH',
     'MAYA_MODULE_PATH',
     'MAYA_PLUG_IN_PATH',
+    'MAYA_SHELF_PATH',
     'PATH',
     'TMPDIR',
     'XBMLANGPATH'
+)
+
+INTERNAL_VARS = (
+    'userAppDir',
+    'userScriptDir',
+    'userPrefDir',
+    'userPresetsDir',
+    'userShelfDir',
+    'userMarkingMenuDir',
+    'userBitmapsDir',
+    'userTmpDir',
+    'userWorkspaceDir',
+    'userHotkeyDir'
 )
 
 OFFSET = '    '
@@ -17,10 +32,24 @@ OFFSET = '    '
 
 def run():
     print
+    print '--- ENVIRONMENT VARIABLES ---'
+    print
     for env in ENV_VARS:
         print env
         value = os.environ.get(env, None)
         if value is None:
+            print OFFSET + 'variable not set.'
+        else:
+            for p in value.split(';'):
+                print OFFSET + p.strip()
+
+    print
+    print '--- INTERNAL VARIABLES ---'
+    print
+    for kwarg in INTERNAL_VARS:
+        print kwarg
+        value = m.internalVar(**{kwarg: True}).strip()
+        if not value:
             print OFFSET + 'variable not set.'
         else:
             for p in value.split(';'):
